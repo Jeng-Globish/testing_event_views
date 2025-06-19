fe43fe4b38842e323c2f85d3f44281b4907b9a61
Jeng <pichitchai@globish.co.th>
2025-06-19 09:18:36 +0000

    test

---
+++ testing_event_views/public/js/custom_event_views2.js
@@ -0,0 +1,29 @@
+frappe.views.calendar["Event"] = {
+	field_map: {
+		start: "starts_on",
+		end: "ends_on",
+		id: "name",
+		allDay: "all_day",
+		title: "subject",
+		status: "event_type",
+		color: "color",
+	},
+
+	style_map: {
+		Public: "success",
+		Private: "info",
+	},
+	filters: [
+	{
+		fieldtype: "Select",
+		fieldname: "Event Type",
+		options: "Public",
+		label: __("Public"),
+	},
+	],
+	// options: {
+	// 	slotDuration: "00:30:00",
+	// 	displayEventEnd: true,
+	// },
+	get_events_method: "frappe.desk.doctype.event.event.get_events",
+};

4d95dabb84b2a0c1392d62a42056b7bd9b49b4e7
Jeng <pichitchai@globish.co.th>
2025-06-24 04:49:08 +0000

    add hooks

--- testing_event_views/hooks.py
+++ testing_event_views/hooks.py
@@ -48,6 +48,7 @@ app_license = "mit"
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"Event" : "public/js/custom_calendar_2.js"}
 # doctype_calendar_js = {"Event" : "public/js/custom_event_views2.js"}
+doctype_calendar_js = {"Event" : "public/js/custom_cloud_calendar.js"}
 
 
 # Svg Icons
